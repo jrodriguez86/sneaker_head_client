2bb8c89a54b178f9ec5c0fbec844d272f870372c
jrodriguez86 <57810392+jrodriguez86@users.noreply.github.com>
2020-05-11 14:45:47 -0400

    React connected to API and Create form working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import NewShoes from "./components/NewShoes.js"
+
+
 
 class App extends React.Component {
+
+state = {
+  shoes: []
+}
+
+componentDidMount() {
+  this.getNotices()
+}
+getNotices () {
+  fetch('http://localhost:3000/shoes')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.error(error))
+}
+
+handleAddRequest = (shoes) => {
+  const copyShoes = [...this.state.shoes]
+  copyShoes.unshift(shoes)
+  this.setState({
+    shoes: copyShoes,
+    name: '',
+    brand: '',
+    year: '',
+  })
+}
+
   render() {
+    console.log(this.state.shoes)
     return (
       <div className="App">
-        <h1>Shoes</h1>
+        <h1>SneakerHead</h1>
+        <NewShoes
+    handleAddRequest={this.handleAddRequest}/>
+        
+
+        
       </div>
     )
   }
---
+++ src/components/NewShoes.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import '../App.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+
+
+class NewShoes extends React.Component {
+    state = {
+        
+            name: '',
+            brand: '',
+            year: '',
+        
+    }
+   
+    handleChange = (event) => {
+        this.setState({ [event.target.id]: event.target.value})
+      }
+
+      handleSubmit = (event) => {
+        event.preventDefault()
+        fetch('http://localhost:3000/shoes', {
+          method: 'POST',
+          body: JSON.stringify({name: this.state.name, brand: this.state.brand, year: this.state.year}),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }).then (res => res.json())
+          .then (resJson => {
+            this.props.handleAddRequest(resJson)
+            this.setState({
+              name: '',
+              brand: '',
+              year: ''
+            })
+        }).catch (error => console.error({'Error': error}))
+      }
+    
+    
+    render() {
+            return (
+              <Jumbotron className='newformjumbo'>
+                 <h1 className="comment-title">Add new Kicks to collection</h1>
+               <Form onSubmit={this.handleSubmit}>
+                 
+                 <Form.Row>
+                   <Form.Control className="newform" type="text" id="name" name="name" onChange={this.handleChange} value={this.state.name} placeholder="Name" />
+                   <Form.Control className="newform" type="text" id="comments" name="comments" onChange={this.handleChange} value={this.state.brand} placeholder="Comment" />
+                   <Form.Control className="newform" type="text" id="location" name="location" onChange={this.handleChange} value={this.state.year} placeholder="Location" />
+                   <br/><br/>
+                   <Button type="submit" value="Submit">Add Kicks</Button>
+              </Form.Row>
+         
+
+               </Form>  
+               </Jumbotron>
+            )
+        }
+}
+
+export default NewShoes
